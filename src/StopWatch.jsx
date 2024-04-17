b4e10a09512ac28db4ba3598d2fed862843fe2f7
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-04-17 13:38:32 +0200

    Added padStart

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -42,6 +42,11 @@ function StopWatch (){
         let seconds = Math.floor(elapsedTime / (1000) % 60);
         let milliseconds = Math.floor((elapsedTime % 1000) / 10);
 
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+
+
         return `${minutes}:${seconds}:${milliseconds}`;
     }
 
