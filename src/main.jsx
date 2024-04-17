27ab1215754b6821a6f73615de3ec644aeb8440d
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-04-17 12:16:39 +0200

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StopWatch from './StopWatch'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StopWatch />
   </React.StrictMode>,
 )
